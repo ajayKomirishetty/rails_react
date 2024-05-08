55185a55a4df2cab9064d80c4d23030cac1e986b
ajaykomirishetty <komirishettyajay@gmail.com>
2024-05-08 09:11:42 -0400

    Integrate flash messages

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,6 +4,7 @@ class AppointmentsController < ApplicationController
     uri = URI('https://jsonplaceholder.typicode.com/todos/2')
     response = Net::HTTP.get(uri)
     @data = JSON.parse(response)
+    flash[:notice] = 'data collected succesfully'
   rescue StandardError => e
     puts "Failed to fetch data: #{e.message}"
   end
